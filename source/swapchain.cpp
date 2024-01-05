6c2467ae4ccec2f5f05a7f6ec88a1df688036a33
FlannyH <flannyha@gmail.com>
2024-01-06 00:37:23 +0100

    Add _frame_index

--- source/swapchain.cpp
+++ source/swapchain.cpp
@@ -15,10 +15,13 @@ namespace gfx {
     ComPtr<IDXGISwapChain3> _swapchain = nullptr;
     ComPtr<ID3D12Resource> _render_targets[backbuffer_count];
     int _frame_wait_values[backbuffer_count];
+    int _frame_index;
 }
 
 namespace gfx {
     void create_swapchain() {
+        _frame_index = 0;
+
         int width, height;
         gfx::get_window_size(width, height);
 
