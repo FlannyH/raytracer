04cd35bcd279739139ea81eac739b7bd1604984f
FlannyH <flannyha@gmail.com>
2024-01-05 22:37:39 +0100

    Add gfx::create_swapchain

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,12 @@
 #include <cstdio>
 #include "device.h"
+#include "swapchain.h"
+#include "command.h"
 
 int main(int n_args, char** args) {
     gfx::create_window(1280, 720);
     gfx::init_device(true);
+    gfx::create_command_queue();
+    gfx::create_swapchain();
     while (1);
 }
--- source/swapchain.cpp
+++ source/swapchain.cpp
@@ -1,6 +1,62 @@
 #include "device.h"
+#include <glfw/glfw3.h>
+#include <glfw/glfw3native.h>
+#include <d3d12.h>
+#include <d3d12sdklayers.h>
+#include <dxgi1_6.h>
+#include <exception>
+#include <wrl.h>
+#include "common.h"
+
+using Microsoft::WRL::ComPtr;
+
+namespace gfx {
+    extern ComPtr<IDXGIFactory4> _factory;
+    extern ComPtr<ID3D12CommandQueue> _command_queue;
+    extern HWND _window_hwnd;
+
+    static constexpr UINT backbuffer_count = 3;
+    ComPtr<IDXGISwapChain3> _swapchain = nullptr;
+    ComPtr<ID3D12Resource> _render_targets[backbuffer_count];
+    int _frame_wait_values[backbuffer_count];
+}
 
 namespace gfx {
     void create_swapchain() {
+        int width, height;
+        gfx::get_window_size(width, height);
+
+        const DXGI_SWAP_CHAIN_DESC1 swapchain_desc = {
+            .Width = static_cast<UINT>(width),
+            .Height = static_cast<UINT>(height),
+            .Format = DXGI_FORMAT_R8G8B8A8_UNORM,
+            .SampleDesc = {
+                .Count = 1,
+            },
+            .BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT,
+            .BufferCount = backbuffer_count,
+            .Scaling = DXGI_SCALING_STRETCH,
+            .SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD,
+        };
+
+        IDXGISwapChain1* new_swapchain;
+        validate(_factory->CreateSwapChainForHwnd(
+            _command_queue.Get(), 
+            _window_hwnd, 
+            &swapchain_desc, 
+            nullptr, 
+            nullptr, 
+            &new_swapchain
+        ));
+
+        const HRESULT swapchain_support = new_swapchain->QueryInterface(__uuidof(IDXGISwapChain3), reinterpret_cast<void**>(&new_swapchain));
+
+        if (SUCCEEDED(swapchain_support)) {
+            _swapchain = static_cast<IDXGISwapChain3*>(new_swapchain);
+        }
+
+        if (!_swapchain) {
+            throw std::exception();
+        }
     }
 }
