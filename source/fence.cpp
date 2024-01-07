ac9071647191806297d47f92d7d3f147e219f812
Flan <flannyha@gmail.com>
2024-01-07 02:10:42 +0100

    Implement Fence

---
+++ source/fence.cpp
@@ -0,0 +1,25 @@
+#include "fence.h"
+#include "device.h"
+#include "command.h"
+
+namespace gfx {
+    Fence::Fence(const Device& device) {
+        device.device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence));
+    }
+
+    void Fence::cpu_wait(const size_t value) {
+        while (m_fence->GetCompletedValue() < value);
+    }
+
+    void Fence::cpu_signal(const size_t value) const {
+        m_fence->Signal(value);
+    }
+
+    void Fence::gpu_wait(const CommandQueue& queue, const size_t value) const {
+        queue.command_queue->Wait(m_fence.Get(), value);
+    }
+
+    void Fence::gpu_signal(const CommandQueue& queue, const size_t value) const {
+        queue.command_queue->Signal(m_fence.Get(), value);
+    }
+}
---
+++ source/fence.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <d3d12.h>
+
+#include "common.h"
+
+namespace gfx {
+    struct CommandQueue;
+    struct Device;
+
+    struct Fence {
+        explicit Fence(const Device& device);
+        void cpu_wait(size_t value);
+        void cpu_signal(size_t value) const;
+        void gpu_wait(const CommandQueue& queue, size_t value) const;
+        void gpu_signal(const CommandQueue& queue, size_t value) const;
+
+    private:
+        ComPtr<ID3D12Fence> m_fence;
+    };
+}
