265704e5669eba5665ab929e0e77f957272faad6
FlannyH <flannyha@gmail.com>
2024-02-27 21:54:21 +0100

    Add SceneNode type implementing the mesh type

--- source/scene.h
+++ source/scene.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <glm/vec3.hpp>
 #include <glm/vec2.hpp>
-#include<glm/gtx/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <memory>
+#include <vector>
+#include "resource.h"
 
 namespace gfx {
     struct Transform {
@@ -11,5 +14,25 @@ namespace gfx {
 
         Transform identity();
         glm::mat4 as_matrix();
+    };
+
+    enum class SceneNodeType : uint8_t {
+        Empty,
+        Mesh,
+    };
+
+    struct SceneNode {
+        Transform local_transform;
+        std::shared_ptr<SceneNode> parent;
+        std::vector<std::shared_ptr<SceneNode>> children;
+
+        SceneNodeType type;
+        union {
+            struct {
+                ResourceID position_buffer;
+                ResourceID attribute_buffer;
+                D3D12_RAYTRACING_GEOMETRY_TRIANGLES_DESC geometry;
+            } mesh;
+        };
     };
 }
