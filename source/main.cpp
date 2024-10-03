e3aa297089672d703ed6b3710e5a4f2a32aa91ff
Lily Haverlag <flannyha@gmail.com>
2024-10-04 00:11:11 +0200

    Implement delta time

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,9 @@
 #include "buffer.h"
-#include <glm/gtx/transform.hpp>
 #include "input.h"
 #include "scene.h"
 #include "renderer.h"
+#include <glm/gtx/transform.hpp>
+#include <chrono>
 
 int main(int n_args, char** args) {
     const auto renderer = std::make_unique<gfx::Renderer>(1280, 720, false);
@@ -11,18 +12,25 @@ int main(int n_args, char** args) {
 
     gfx::Transform camera;
     glm::vec3 camera_euler_angles(0.0f);
-    float move_speed = 0.01f;
+    float move_speed = 1.0f;
     float mouse_sensitivity = 0.004f;
 
+    auto prev_time = std::chrono::steady_clock::now();
+
     while (renderer->should_stay_open()) {
+        auto curr_time = std::chrono::steady_clock::now();
+        std::chrono::duration<float> duration = curr_time - prev_time;
+        float delta_time = duration.count();
+        prev_time = curr_time;
+
         input::update();
 
-        if (input::key_held(input::Key::w)) camera.position += camera.forward_vector() * move_speed;
-        if (input::key_held(input::Key::s)) camera.position -= camera.forward_vector() * move_speed;
-        if (input::key_held(input::Key::d)) camera.position += camera.right_vector() * move_speed;
-        if (input::key_held(input::Key::a)) camera.position -= camera.right_vector() * move_speed;
-        if (input::key_held(input::Key::space)) camera.position.y += move_speed;
-        if (input::key_held(input::Key::left_shift)) camera.position.y -= move_speed;
+        if (input::key_held(input::Key::w)) camera.position += camera.forward_vector() * move_speed * delta_time;
+        if (input::key_held(input::Key::s)) camera.position -= camera.forward_vector() * move_speed * delta_time;
+        if (input::key_held(input::Key::d)) camera.position += camera.right_vector() * move_speed * delta_time;
+        if (input::key_held(input::Key::a)) camera.position -= camera.right_vector() * move_speed * delta_time;
+        if (input::key_held(input::Key::space)) camera.position.y += move_speed * delta_time;
+        if (input::key_held(input::Key::left_shift)) camera.position.y -= move_speed * delta_time;
         if (input::mouse_button(input::MouseButton::right)) {
             camera_euler_angles.y -= input::mouse_movement().x * mouse_sensitivity;
             camera_euler_angles.x -= input::mouse_movement().y * mouse_sensitivity;
