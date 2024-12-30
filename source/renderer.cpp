83e4d5cb52bb34869dda8b074e94daa96f36a9df
Lily Haverlag <flannyha@gmail.com>
2024-12-30 03:25:53 +0100

    Rename roughness_metallic -> metallic_roughness

--- source/renderer.cpp
+++ source/renderer.cpp
@@ -21,14 +21,14 @@ namespace gfx {
         m_device = std::make_unique<Device>(width, height, debug_layer_enabled);
         m_color_target = m_device->create_render_target("Color framebuffer", width, height, PixelFormat::rgba16_float, glm::vec4(0.0f, 0.0f, 0.0f, -1.0f), ResourceUsage::compute_write);
         m_normal_target = m_device->create_render_target("Normal framebuffer", width, height, PixelFormat::rgba16_float, {}, ResourceUsage::compute_write);
-        m_roughness_metallic_target = m_device->create_render_target("Roughness framebuffer", width, height, PixelFormat::rg8_unorm, {}, ResourceUsage::compute_write);
+        m_metallic_roughness_target = m_device->create_render_target("Metallic & roughness framebuffer", width, height, PixelFormat::rg8_unorm, {}, ResourceUsage::compute_write);
         m_emissive_target = m_device->create_render_target("Emissive framebuffer", width, height, PixelFormat::rg11_b10_float, {}, ResourceUsage::compute_write);
         m_shaded_target = m_device->load_texture("Shaded framebuffer", width, height, 1, nullptr, PixelFormat::rgba16_float, TextureType::tex_2d, ResourceUsage::compute_write);
         m_depth_target = m_device->create_depth_target("Depth framebuffer", width, height, PixelFormat::depth32_float);
         m_pipeline_scene = m_device->create_raster_pipeline("assets/shaders/geo_pass.vs.hlsl", "assets/shaders/geo_pass.ps.hlsl", {
             m_color_target,
             m_normal_target,
-            m_roughness_metallic_target,
+            m_metallic_roughness_target,
             m_emissive_target
         }, m_depth_target);
         m_pipeline_brdf = m_device->create_compute_pipeline("assets/shaders/brdf.cs.hlsl");
@@ -115,7 +115,7 @@ namespace gfx {
             };
             resize_texture(m_color_target, (uint32_t)m_render_resolution.x, (uint32_t)m_render_resolution.y);
             resize_texture(m_normal_target, (uint32_t)m_render_resolution.x, (uint32_t)m_render_resolution.y);
-            resize_texture(m_roughness_metallic_target, (uint32_t)m_render_resolution.x, (uint32_t)m_render_resolution.y);
+            resize_texture(m_metallic_roughness_target, (uint32_t)m_render_resolution.x, (uint32_t)m_render_resolution.y);
             resize_texture(m_emissive_target, (uint32_t)m_render_resolution.x, (uint32_t)m_render_resolution.y);
             resize_texture(m_shaded_target, (uint32_t)m_render_resolution.x, (uint32_t)m_render_resolution.y);
             resize_texture(m_depth_target, (uint32_t)m_render_resolution.x, (uint32_t)m_render_resolution.y);
@@ -126,7 +126,7 @@ namespace gfx {
             .color_targets = {
                 m_color_target,
                 m_normal_target,
-                m_roughness_metallic_target,
+                m_metallic_roughness_target,
                 m_emissive_target,
             },
             .depth_target = m_depth_target,
@@ -158,7 +158,7 @@ namespace gfx {
             { m_shaded_target, ResourceUsage::compute_write },
             { m_color_target, ResourceUsage::non_pixel_shader_read },
             { m_normal_target, ResourceUsage::non_pixel_shader_read },
-            { m_roughness_metallic_target, ResourceUsage::non_pixel_shader_read },
+            { m_metallic_roughness_target, ResourceUsage::non_pixel_shader_read },
             { m_emissive_target, ResourceUsage::non_pixel_shader_read },
             { m_lights_buffer, ResourceUsage::non_pixel_shader_read },
             { m_spherical_harmonics_buffer, ResourceUsage::non_pixel_shader_read },
@@ -169,7 +169,7 @@ namespace gfx {
             m_shaded_target.handle.as_u32_uav(),
             m_color_target.handle.as_u32(),
             m_normal_target.handle.as_u32(),
-            m_roughness_metallic_target.handle.as_u32(),
+            m_metallic_roughness_target.handle.as_u32(),
             m_emissive_target.handle.as_u32(),
             m_lights_buffer.handle.as_u32(),
             m_spherical_harmonics_buffer.handle.as_u32(),
