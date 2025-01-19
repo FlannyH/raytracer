1f50961ae853e323a8cb76922d962746d732cfea
Lily Haverlag <flannyha@gmail.com>
2025-01-19 17:18:30 +0100

    Factor out global root signature to its own function

--- source/pipeline.h
+++ source/pipeline.h
@@ -9,13 +9,16 @@ namespace gfx {
 
     struct Pipeline {
         explicit Pipeline(const Device& device, const std::string& name, const std::string& vertex_shader_path, const std::string& pixel_shader_path, const std::vector<DXGI_FORMAT> render_target_formats, const DXGI_FORMAT depth_target_format = DXGI_FORMAT_UNKNOWN);
-        explicit Pipeline(const Device& device, const std::string& name, const std::string& compute_shader_path);
+        explicit Pipeline(const Device &device, const std::string &name, const std::string &compute_shader_path);
         const std::string& get_name() { return name; }
 
     public:
         ComPtr<ID3D12PipelineState> pipeline_state;
         ComPtr<ID3D12RootSignature> root_signature;
         std::string name;
+
+    private:
+        void create_global_root_signature(const gfx::Device &device);
 
     };
 }
