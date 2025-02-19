e5790191233cf3607804fa6ba06ce3bbce5cae2f
Lily Haverlag <flannyha@gmail.com>
2025-02-20 00:44:20 +0100

    Scale specular IBL to be closer to the reference PT
    it was bothering me

--- assets/shaders/rasterized/brdf.cs.hlsl
+++ assets/shaders/rasterized/brdf.cs.hlsl
@@ -210,7 +210,7 @@ void main(uint3 dispatch_thread_id : SV_DispatchThreadID) {
         float3 env_sample = ibl_texture.SampleLevel(cube_sampler, normalize(rotate_vector_by_quaternion(reflect_dir, view_data.forward)), mip_level).rgb;
         float2 env_brdf = env_brdf_lut.Sample(tex_sampler_clamp, float2(n_dot_v, roughness));
         float3 indirect_specular = env_sample * (specular_f * env_brdf.x + env_brdf.y);
-        specular += specular_f * indirect_specular * FULLBRIGHT_NITS;
+        specular += specular_f * indirect_specular * FULLBRIGHT_NITS * PI;
     }
     
     out_value += diffuse * diffuse_mul * ssao;
